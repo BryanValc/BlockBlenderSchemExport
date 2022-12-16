e1b7a83cafbd134441dd44ce96858323bf11aa25
Bryan Valdez <bryan.valdez117@outlook.es>
2022-12-16 16:37:14 -0600

    The script that adds the "file>export>Export CSV option" with its functionality was added

---
+++ __init__.py
@@ -0,0 +1,44 @@
+import bpy
+import csv
+import math
+from bpy_extras.io_utils import ExportHelper
+
+def write_csv(context, filepath):
+    dg = context.evaluated_depsgraph_get()
+    eval_ob = context.object.evaluated_get(dg)
+    instances = [i for i in dg.object_instances if i.is_instance and i.parent == eval_ob]
+
+    positions = []
+
+    for instance in dg.object_instances:
+        if instance.is_instance and instance.parent == eval_ob:
+            positions.append((
+        instance.object.name,
+        int(((instance.object.matrix_local.translation[0]*2)/eval_ob.scale[0])*math.floor(eval_ob.scale[0])), 
+        int(((instance.object.matrix_local.translation[1]*2)/eval_ob.scale[0])*math.floor(eval_ob.scale[1])),
+        int(((instance.object.matrix_local.translation[2]*2)/eval_ob.scale[0])*math.floor(eval_ob.scale[2]))))
+
+    with open(filepath, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(positions)
+
+class ExportCSV(bpy.types.Operator, ExportHelper):
+    """Export object names and positions to a CSV file"""
+    bl_idname = "export_csv.some_data"
+    bl_label = "Export CSV"
+
+    filename_ext = ".csv"
+
+    def execute(self, context):
+        write_csv(context, self.filepath)
+        return {'FINISHED'}
+
+# register the export operator
+bpy.utils.register_class(ExportCSV)
+
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportCSV.bl_idname, text="Export CSV")
+    
+# add to a menu
+bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
