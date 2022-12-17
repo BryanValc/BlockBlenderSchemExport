0a10da20368f2788a94100852f1ed9ffedbfd741
Bryan Valdez <bryan.valdez117@outlook.es>
2022-12-16 21:19:41 -0600

    now, the script converts to .schem instead of .csv

--- __init__.py
+++ __init__.py
@@ -1,43 +1,56 @@
 import bpy
-import csv
+import subprocess
+
 from bpy_extras.io_utils import ExportHelper
 
-def write_csv(context, filepath):
+python_path = bpy.app.binary_path.replace("blender.exe", "3.4\\python\\bin\\python.exe")
+subprocess.call([python_path, "-m", "ensurepip"])
+subprocess.call([python_path, "-m", "pip", "install", "--upgrade", "pip"])
+subprocess.call([python_path, "-m", "pip", "install", "mcschematic"])
+
+from mcschematic import mcschematic
+
+def write_schematic(context, filepath):
     dg = context.evaluated_depsgraph_get()
     eval_ob = context.object.evaluated_get(dg)
-    instances = [i for i in dg.object_instances if i.is_instance and i.parent == eval_ob]
+    # # instances = [i for i in dg.object_instances if i.is_instance and i.parent == eval_ob]
 
-    positions = []
+    # positions = []
+
+    schematic = mcschematic.MCSchematic()
 
     for instance in dg.object_instances:
         if instance.is_instance and instance.parent == eval_ob:
-            positions.append((
-        instance.object.name,
-        int(((instance.object.matrix_local.translation[0]*2)/eval_ob.scale[0])*round(eval_ob.scale[0])), 
-        int(((instance.object.matrix_local.translation[1]*2)/eval_ob.scale[0])*round(eval_ob.scale[1])),
-        int(((instance.object.matrix_local.translation[2]*2)/eval_ob.scale[0])*round(eval_ob.scale[2]))))
+            schematic.setBlock((
+            int(((((instance.object.matrix_local.translation[0])/eval_ob.scale[0])*round(eval_ob.scale[0]))/instance.object.scale[0])),
+            int(((((instance.object.matrix_local.translation[2])/eval_ob.scale[0])*round(eval_ob.scale[2]))/instance.object.scale[2])),
+            int(((((instance.object.matrix_local.translation[1])/eval_ob.scale[0])*round(eval_ob.scale[1]))/instance.object.scale[1]))
+            ), "minecraft:"+instance.object.name)
 
-    with open(filepath, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows(positions)
+    fullPath = filepath.replace("\\", "/").split("/")
+    path = "/".join(fullPath[:-1])
+    name = fullPath[-1]
+    name.replace(".schem", "")
 
-class ExportCSV(bpy.types.Operator, ExportHelper):
+    schematic.save(path, name, mcschematic.Version.JE_1_18_2)
+
+class ExportSCHEMATIC(bpy.types.Operator, ExportHelper):
     """Export object names and positions to a CSV file"""
-    bl_idname = "export_csv.some_data"
-    bl_label = "Export CSV"
+    bl_idname = "export_schematic.some_data"
+    bl_label = "Export Schematic"
 
-    filename_ext = ".csv"
+    filename_ext = ".schem"
 
     def execute(self, context):
-        write_csv(context, self.filepath)
+        write_schematic(context, self.filepath)
         return {'FINISHED'}
 
 # register the export operator
-bpy.utils.register_class(ExportCSV)
+bpy.utils.register_class(ExportSCHEMATIC)
 
 
 def menu_func_export(self, context):
-    self.layout.operator(ExportCSV.bl_idname, text="Export CSV")
+    self.layout.operator(ExportSCHEMATIC.bl_idname, text="Export schematic")
     
 # add to a menu
 bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
