a99fe450344efb161b4e074615396063db7c9b35
Bryan Valdez <bryan.valdez117@outlook.es>
2022-12-17 22:59:51 -0600

    fixed issue with the resolution, now matrix_world is used instead of the local matrix

--- __init__.py
+++ __init__.py
@@ -19,26 +19,34 @@ bl_info = {
     "tracker_url": "https://github.com/BryanValc/BlockBlenderCSVExport/issues",
     "category": "Import-Export"}
 
+
 def errorObjectNotSelected(self, context):
-    self.layout.label(text= "You have to select an object!")
+    self.layout.label(text="You have to select an object!")
+
 
 def warningRotation(self, context):
-    self.layout.label(text= "It's not recommended to rotate the object when exporting to .schem, you should apply all the transforms!")
+    self.layout.label(
+        text="It's not recommended to rotate the object when exporting to .schem, you should apply all the transforms!")
+
 
 def warningScale(self, context):
-    self.layout.label(text= "It's not recommended to scale the object when exporting to .schem, you should apply all the transforms!")
+    self.layout.label(
+        text="It's not recommended to scale the object when exporting to .schem, you should apply all the transforms!")
 
 
 def write_schematic(context, filepath, version):
     dg = context.evaluated_depsgraph_get()
     eval_ob = context.object.evaluated_get(dg)
-        
-    if(eval_ob is None):
-        bpy.context.window_manager.popup_menu(errorObjectNotSelected, title="Error", icon='ERROR')
-    elif(eval_ob.rotation_euler[0] != 0 or eval_ob.rotation_euler[1] != 0 or eval_ob.rotation_euler[2] != 0):
-        bpy.context.window_manager.popup_menu(warningRotation, title="Error", icon='ERROR')
-    elif(eval_ob.scale[0] != 1 or eval_ob.scale[1] != 1 or eval_ob.scale[2] != 1):
-        bpy.context.window_manager.popup_menu(warningScale, title="Error", icon='ERROR')
+
+    if (eval_ob is None):
+        bpy.context.window_manager.popup_menu(
+            errorObjectNotSelected, title="Error", icon='ERROR')
+    elif (eval_ob.rotation_euler[0] != 0 or eval_ob.rotation_euler[1] != 0 or eval_ob.rotation_euler[2] != 0):
+        bpy.context.window_manager.popup_menu(
+            warningRotation, title="Error", icon='ERROR')
+    elif (eval_ob.scale[0] != 1 or eval_ob.scale[1] != 1 or eval_ob.scale[2] != 1):
+        bpy.context.window_manager.popup_menu(
+            warningScale, title="Error", icon='ERROR')
     else:
         schematic = mcschematic.MCSchematic()
 
@@ -46,11 +54,11 @@ def write_schematic(context, filepath, version):
             if instance.is_instance and instance.parent == eval_ob:
                 schematic.setBlock((
                     int(((instance.object.matrix_local.translation[0]+(
-                        instance.object.scale[0]/2))/instance.object.scale[0])/eval_ob.scale[0]),
+                        instance.object.matrix_world.to_scale()[0]/2))/instance.object.matrix_world.to_scale()[0])/eval_ob.scale[0]),
                     int(((instance.object.matrix_local.translation[2]+(
-                        instance.object.scale[2]/2))/instance.object.scale[2])/eval_ob.scale[2]),
+                        instance.object.matrix_world.to_scale()[2]/2))/instance.object.matrix_world.to_scale()[2])/eval_ob.scale[2]),
                     -int(((instance.object.matrix_local.translation[1]+(
-                        instance.object.scale[1]/2))/instance.object.scale[1])/eval_ob.scale[1]),
+                        instance.object.matrix_world.to_scale()[1]/2))/instance.object.matrix_world.to_scale()[1])/eval_ob.scale[1]),
                 ), "minecraft:"+instance.object.name)
 
         fullPath = filepath.replace("\\", "/").split("/")
