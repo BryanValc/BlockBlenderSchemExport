aaf6524ccaadbc3e1cb6e815a16ada5a6d0b9ba7
Bryan Valdez <bryan.valdez117@outlook.es>
2022-12-21 10:08:43 -0600

    Minecraft x and z offsets now match BlockBlender 1.2 version

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ from . import mcschematic, nbtlib, immutable_views
 bl_info = {
     "name": "BlockBlender to .schem export",
     "author": "Bryan Valdez",
-    "version": (1, 0, 1),
+    "version": (1, 2, 0),
     "blender": (3, 4, 0),
     "location": "File > Export > Export Minecraft .schem",
     "description": "add-on that converts the selected object affected by the geometry node shown in this video www.youtube.com/watch?v=TUw65gz8nOs",
@@ -56,7 +56,7 @@ def write_schematic(context, filepath, version):
                 name = "minecraft:"+block_list.get_block(nameId)
                 schematic.setBlock((
                     int((pos[0]+(min_distance/2))/min_distance),
-                    int((pos[2]+(min_distance))/min_distance),
+                    int((pos[2]+(min_distance/2))/min_distance),
                     -int((pos[1]+(min_distance/2))/min_distance)
                 ), name)
         else:
@@ -71,7 +71,7 @@ def write_schematic(context, filepath, version):
                     scale = instance.object.matrix_world.to_scale()
                     schematic.setBlock((
                         int((translation[0]+(scale[0]/2))/scale[0]),
-                        int((translation[2]+(scale[2]))/scale[2]),
+                        int((translation[2]+(scale[2]/2))/scale[2]),
                         -int((translation[1]+(scale[1]/2))/scale[1]),
                     ), "minecraft:"+convertedName)
 
